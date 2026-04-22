4bd90d3b3ee312ffe80a97d8f0d5b2aa85d7da86
shivamparihar2551 <shivamsinghparihar2323@gmail.com>
2026-04-22 23:16:35 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,18 +1,136 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-# Load dataset
-df = pd.read_csv("dynamic_supply_chain_logistics_dataset.csv")
-
-# Display basic info
-print(df.head())
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+df = pd.read_csv("C:/Users/kr360/Downloads/AQI Dataset for project.csv")
+#pd.set_option('display.max_rows', 5000)
+#pd.set_option('display.max_columns', 50)
+#print(df.head(100))
+print(df.describe())
 print(df.info())
+print(df.isna().sum())
+df = df.dropna()
+print(df.isna().sum())
+df['last_update'] = pd.to_datetime(df['last_update'], errors='coerce')
+print(df.info())
+df['city'] = df['city'].str.lower()
+df['state'] = df['state'].str.lower()
+df["Pollution Level"] = None
+df.loc[df["pollutant_avg"]<50, "Pollution Level"] = "Good"
+df.loc[((df["pollutant_avg"]>=50) & (df["pollutant_avg"]<100)), "Pollution Level"] = "Moderate"
+df.loc[df["pollutant_avg"]>=100, "Pollution Level"] = "Unhealthy"
+print(df.head(10))
+print(df.info())
+
+
+# Detecting outliers and inliers
+plt.figure(figsize=(10,6))
+sns.boxplot(x='pollutant_id', y='pollutant_avg', data=df)
+plt.title("Box Plot of Pollutants")
+plt.xlabel("Pollutant Type")
+plt.ylabel("Average Value")
+plt.xticks(rotation=45)
+plt.show()
+
+
+numeric_cols = ["pollutant_min", "pollutant_max", "pollutant_avg"]
+outliers = []
+for col in numeric_cols:
+    Q1 = df[col].quantile(0.25)      # It marks the point below which 25% of the data falls.
+    Q3 = df[col].quantile(0.75)      # It marks the point below which 75% of the data falls.
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    outliers.extend(df[(df[col] < lower_bound) | (df[col] > upper_bound)].index)
+
+print(outliers)
+s1 = df.drop(set(outliers))
+print(s1.info())
+
+
+# Normalising the data
+df[numeric_cols] = (df[numeric_cols] - df[numeric_cols].min()) / (df[numeric_cols].max() - df[numeric_cols].min())
+print(df)
 print(df.describe())
 
 
-print("Shape:", df.shape)
-print("Columns:", df.columns)
+# Visualization
+# Plot 1
+df.groupby('pollutant_id')['pollutant_avg'].mean().plot(kind='bar')
+plt.title("Average Pollution by Pollutant")
+plt.xlabel("Pollutant")
+plt.ylabel("Average Value")
+plt.show()
 
-print(df.isnull().sum())
+
+# Plot 2
+top_states = df.groupby('state')['pollutant_avg'].mean().sort_values(ascending=False).head(10)
+top_states.plot(kind='bar')
+plt.title("Top 10 Polluted States")
+plt.show()
+
+
+# Plot 3
+counts = df['Pollution Level'].value_counts()
+plt.figure()
+plt.pie(counts, labels=counts.index, autopct='%1.1f%%')
+plt.title("Pollution Level Distribution")
+plt.show()
+
+
+
+# Plot 4   // Scatter plot which show pollution changes with geographical position. You may see clusters (north vs south differences)
+plt.figure(figsize=(10, 5))
+sns.scatterplot(x='latitude', y='pollutant_avg', hue='Pollution Level', data=df, palette='pastel')
+plt.xlabel("Latitude")
+plt.ylabel("Pollution Level")
+plt.title("Pollution vs Latitude")
+plt.show()
+
+
+# Plot 5
+plt.figure()
+sns.heatmap(df[['pollutant_min','pollutant_max','pollutant_avg']].corr(), annot=True, cmap='coolwarm', linewidths=0.5)
+plt.title("Correlation Between Pollutants")
+plt.show()
+
+
+# Plot 6
+plt.figure()
+plt.hist(df['pollutant_avg'], bins=20, color='purple', edgecolor='black', alpha=0.3)
+plt.title("Distribution of Pollution Levels")
+plt.xlabel("Pollution")
+plt.ylabel("Frequency")
+plt.show()
+
+
+
+
+# Linear Regression
+X = df[['latitude']]
+y = df[['pollutant_avg']]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Predict
+#checkTarget = pd.DataFrame({'bmi':[25]})
+#result = model.predict(checkTarget)
+#print("Predicted Target for bmi 25: ", result)
+
+
+
+plt.figure()
+plt.scatter(X, y, color="blue")
+plt.plot(X, model.predict(X), color='red', linewidth=3)
+plt.xlabel('latitude')
+plt.ylabel('Pollution Avg')
+plt.title('Linear Regression Fit')
+plt.show()
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+print("MSE:", mse)
